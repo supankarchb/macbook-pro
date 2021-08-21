2e757686e5e06423d3978e130c41956b6b67ccaf
supankarchb <supankar710@gmail.com>
2021-08-21 19:22:53 +0600

    all

--- mac.js
+++ mac.js
@@ -1,86 +1,47 @@
-// function memoryCosting(quantityMemory,price){
-
-//     const extraEightgbmemoryCosting = document.getElementById(quantityMemory + '-gb-memory');
-//     const memoryCostingField = document.getElementById('extra-memory-cost');
-//     memoryCostingField.innerText = price;
-//     updateTotalPricing();
-   
-
-// }
-// document.getElementById('eight-gb-memory').addEventListener('click', function(){
-
-//     memoryCosting('eight',0);
-// })
-// document.getElementById('sixteen-gb-memory').addEventListener('click', function(){
-
-//     memoryCosting('sixteen',180);
-// })
-
-
-// function getInputValue(quantityMemory){
-//     const productInput = document.getElementById(quantityMemory + '-gb-memory');
-//     const productNumber = parseInt(productInput.value);
-//     return productNumber;
-
-
 const extraEightgbmemoryCosting = document.getElementById('eight-gb-memory');
 const extraSixteengbmemoryCosting = document.getElementById('sixteen-gb-memory');
 const memoryCostingField = document.getElementById('extra-memory-cost');
 
 extraEightgbmemoryCosting.addEventListener('click', function(){
-
     memoryCostingField.innerText = '0';
     updateTotalPricing();
-
 })
 extraSixteengbmemoryCosting.addEventListener('click', function(){
-
     memoryCostingField.innerText = '180';
     updateTotalPricing();
-
 });
 const baseSsdCosting = document.getElementById('base-ssd-storage');
 const halfOfOneTeraCosting = document.getElementById('halfofonetera-ssd-storage');
 const oneTeraCosting = document.getElementById('onetera-ssd-storage');
 const extarSsdCosting= document.getElementById('extra-ssd-cost');
 
-
 baseSsdCosting.addEventListener('click', function(){
-
     extarSsdCosting.innerText = '0';
     updateTotalPricing();
-
 })
 halfOfOneTeraCosting.addEventListener('click', function(){
-
     extarSsdCosting.innerText = '100';
     updateTotalPricing();
 })
-
 oneTeraCosting.addEventListener('click', function(){
-
     extarSsdCosting.innerText = '180';
     updateTotalPricing();
-
 })
-
 const freeDelivey = document.getElementById('free-delivery');
 const chargedDelivery = document.getElementById('extra-delivery-added');
 const shippingFeild = document.getElementById('extra-deliverycost-added');
 
 freeDelivey.addEventListener('click', function(){
-
     shippingFeild.innerText = '0';
     updateTotalPricing();
-
 })
 chargedDelivery.addEventListener('click', function(){
     shippingFeild.innerText = '20';
     updateTotalPricing();
 
 });
-
 const totalPricing = document.getElementById('total-pricing');
+const newGrandTotal = document.getElementById('grand-total');
 
 function updateTotalPricing(){
 
@@ -90,4 +51,14 @@ function updateTotalPricing(){
 
     const grandTotal = 1299 + extraMemoryCharge + extraSsdCharge + deliveryCharge;
     totalPricing.innerText = grandTotal; 
+    const newGrandTotals = grandTotal;
+    newGrandTotal.innerText = newGrandTotals;
+};
+function verifyPromoCode(){
+    const typedpromocode = document.getElementById('enter-promo-code').value;
+
+    if(typedpromocode == 'stevekaku'){
+        newGrandTotal.innerText = parseInt(totalPricing.innerText) - (( parseInt(totalPricing.innerText))*(20 / 100));
+        typedpromocode.inner = '';
+    }
 }
